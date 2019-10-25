22e61eabb4c15f2fb4e1978345cbb92cb94ec7c8
Joseph Miller <jdworldtv@gmail.com>
2019-10-25 03:31:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,8 @@
+def introduction (name) 
+  puts "Hi, my name is #{name}."
+  name = "Josh"
+end
 
-  1) #introduction takes in an argument of a name and puts out a phrase with that name using string interpolation
-  
-  def interpolation(name)
-    
-   puts "Hi, my name is #{name}."
-   
-   (name) = "Josh"
-   
-   
-  end
+def introduction_with_language(name, language) 
+  puts"Hi, my name is #{name} and I am learning to program in #{language}"
+end
